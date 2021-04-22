5fc6afc1ff6fe2112e00848922eb1a52cc5bde52
Chris Guida <chrisguida@gmail.com>
2021-04-22 17:07:56 -0600

    change all timeouts of 10s to 30s

--- synapse/handlers/federation.py
+++ synapse/handlers/federation.py
@@ -519,8 +519,8 @@ class FederationHandler(BaseHandler):
                 earliest_events_ids=list(latest),
                 latest_events=[pdu],
                 limit=10,
-                min_depth=min_depth,s
-                timeout=1000000,
+                min_depth=min_depth,
+                timeout=60000,
             )
         except (RequestSendFailed, HttpResponseException, NotRetryingDestination) as e:
             # We failed to get the missing events, but since we need to handle
@@ -2374,7 +2374,7 @@ class FederationHandler(BaseHandler):
                 e_id,
                 room_version=room_version,
                 outlier=True,
-                timeout=10000,
+                timeout=30000,
             )
             if m_ev and m_ev.event_id == e_id:
                 event_map[e_id] = m_ev
